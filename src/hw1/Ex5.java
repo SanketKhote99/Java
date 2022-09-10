72acf7b294644461a35fad658dcd5a075b9b051e
Sanket Khote <sanketkhote43@gmail.com>
2022-09-10 19:56:10 +0530

    practical add

---
+++ src/hw1/Ex5.java
@@ -0,0 +1,10 @@
+package hw1;
+import java.util.Scanner;
+public class Ex5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter radius ");
+        double num = sc.nextDouble();
+        System.out.println("Area of Circle = "+3.14*num*num);
+      }
+    }
