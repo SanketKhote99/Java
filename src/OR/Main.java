44feb1584bdd1c6dc0ea97f07e97b2e9def29558
Sanket Khote <sanketkhote43@gmail.com>
2022-09-11 10:50:52 +0530

    overriding example add

---
+++ src/OR/Main.java
@@ -0,0 +1,9 @@
+package OR;
+
+public class Main{
+    public static void main(String[] args) {
+        Aniket sk = new Aniket();
+        sk.Sanket();
+    }
+    
+}
---
+++ src/OR/Parent.java
@@ -0,0 +1,20 @@
+package OR;
+
+import javax.sound.sampled.SourceDataLine;
+
+public class Parent {
+    public void Bike(){
+        System.out.println("TVS Starcity ");
+    }
+    
+   public void Sanket(){
+    System.out.println("hunter");
+}
+}
+
+ class Aniket extends Parent{
+    public void Bk(){
+    System.out.println("Mt-15 v2");
+
+    }
+ }
