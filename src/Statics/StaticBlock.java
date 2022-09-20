50bee4d4fe09016af41394609ddc968b20b5429a
Sanket Khote <sanketkhote43@gmail.com>
2022-09-20 17:06:36 +0530

    static block

---
+++ src/Statics/StaticBlock.java
@@ -0,0 +1,15 @@
+package Statics;
+public class StaticBlock{
+
+   void Demo(){
+    System.out.println("Void Demo Block");
+    }
+    static{
+        System.out.println("Hello Everyone");
+       
+    }
+
+    public static void main(String[] args) {
+       
+    }
+}
